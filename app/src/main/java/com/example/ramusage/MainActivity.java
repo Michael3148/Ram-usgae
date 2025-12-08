23765d5312d9fbde635cbcd8e57f1ec201838363
michael teferi <michaelteferi97@gmail.com>
2025-12-08 23:44:31 +0300

    Total ram , ram usage , and ram free ram

--- app/src/main/java/com/example/ramusage/MainActivity.java
+++ app/src/main/java/com/example/ramusage/MainActivity.java
@@ -1,24 +1,62 @@
 package com.example.ramusage;
 
+import android.app.ActivityManager;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.ramusage.databinding.ActivityMainBinding;
 
+import java.lang.reflect.Method;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        long totalRam = getTotalRAM();
+        double totalRamInGB = totalRam / 1024.0 / 1024.0 / 1024.0;
+
+        long freeRam = getFreeRAM();
+        double freeRamInGB = freeRam / 1024.0 / 1024.0 / 1024.0;
+
+        long usedRam = totalRam - freeRam; // used RAM in bytes
+        double usedRamInGB = usedRam / 1024.0 / 1024.0 / 1024.0;
+
+        binding.textView5.setText(String.format("Used RAM: %.2f GB",usedRamInGB));
+        binding.textView3.setText(String.format("Free RAM: %.2f GB",freeRamInGB));
+        binding.textView4.setText(String.format("Total RAM: %.2f GB",totalRamInGB));
+    }
+
+    private long getTotalRAM() {
+        ActivityManager actManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        ActivityManager.MemoryInfo memInfo = new ActivityManager.MemoryInfo();
+        actManager.getMemoryInfo(memInfo);
+
+        // Total RAM in bytes
+        return memInfo.totalMem;
+    }
+    private long getFreeRAM() {
+        ActivityManager activityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        ActivityManager.MemoryInfo memInfo = new ActivityManager.MemoryInfo();
+        activityManager.getMemoryInfo(memInfo);
+
+        // Available RAM in bytes
+        return memInfo.availMem;
     }
 }
